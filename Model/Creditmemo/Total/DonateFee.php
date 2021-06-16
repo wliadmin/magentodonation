e7737e3ebcd40b9e966c6ca0b42853baf4124757
priyajp <priyajp@weblineapps.com>
2021-06-16 17:10:22 +0530

    fix: the corrected changes

--- Model/Creditmemo/Total/DonateFee.php
+++ Model/Creditmemo/Total/DonateFee.php
@@ -5,8 +5,8 @@ use Magento\Sales\Model\Order\Creditmemo;
 use Magento\Sales\Model\Order\Creditmemo\Total\AbstractTotal;
 
 /**
- * Class Fee
- * @package Prince\Extrafee\Model\Creditmemo\Total
+ * Class DonateFee
+ * @package Webline\Donation\Model\Creditmemo\Total
  */
 class DonateFee extends AbstractTotal
 {
